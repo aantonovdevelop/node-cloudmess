cfd612e1eff93b70ed7e71aacf0267cd7e6d2230
Artem Antonov <aantonov.develop@gmail.com>
2016-03-25 23:22:32 +0500

    Fix MessageSender tests

--- tests/message-sender.js
+++ tests/message-sender.js
@@ -8,7 +8,8 @@ var gcm = require('./mocks/gcm');
 describe('MessageSender', function () {
     describe('#send_message', function () {
         it('Should create message and send it to gcm sender', function (done) {
-            var message_sender = new MessageSender(gcm);
+            var gcm_api_key = 'test_api_key';
+            var message_sender = new MessageSender(gcm, gcm_api_key);
 
             var keys = ['test_key'];
             var message = {
